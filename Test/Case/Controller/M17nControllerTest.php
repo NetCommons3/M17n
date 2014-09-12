b3ef6f53fc8bce92c0d60459a8e62f1c0a319968
topaz2 <topaz2.3333@gmail.com>
2014-09-12 18:59:10 +0900

    Added mock

--- Test/Case/Controller/M17nControllerTest.php
+++ Test/Case/Controller/M17nControllerTest.php
@@ -1,16 +1,25 @@
 <?php
 /**
+ * Summary for M17nController Test Case
+ */
+
+App::uses('M17nController', 'Controller');
+
+/**
  * M17nController Test Case
  *
  * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
  * @link     http://www.netcommons.org NetCommons Project
  * @license  http://www.netcommons.org/license.txt NetCommons License
  */
-
-App::uses('M17nController', 'Controller');
+class M17nControllerTest extends ControllerTestCase {
 
 /**
- * Summary for M17nController Test Case
+ * test index
+ *
+ * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
+ * @return   void
  */
-class M17nControllerTest extends ControllerTestCase {
+	public function testIndex() {
+	}
 }
